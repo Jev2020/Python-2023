d49b0500e6195495c7d5bbec5dae0eab282a6678
Jevhenij Kuzmenko <kjevhenij@gmail.com>
2023-04-24 14:12:43 +0200

    Ukol

---
+++ ukol.4.py
@@ -0,0 +1,15 @@
+predvolba = '+420'
+text_jedne_zpravy = 180
+cena_jedne_zpravy = '3 Kc'
+
+
+cislo_uzivatele = int(input('Zadej cislo uzivatele:'))
+def cislo_uzivatele():
+    if cislo_uzivatele == 9 or cislo_uzivatele[:4] == predvolba:
+        return True
+        text_zpravy = ('Zadej text zpravy:')
+        def text_zpravy():
+             cena_jedne_zpravy = text_jedne_zpravy
+             cena_zpravy = len(text_zpravy) // text_jedne_zpravy
+    else:
+        return False
