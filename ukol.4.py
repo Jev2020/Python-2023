bc06ccd0009c9623d74935e357220096a8dbe26e
Jevhenij Kuzmenko <kjevhenij@gmail.com>
2023-05-03 15:45:03 +0200

    Opraveny ukol

--- ukol.4.py
+++ ukol.4.py
@@ -4,12 +4,28 @@ cena_jedne_zpravy = '3 Kc'
 
 
 cislo_uzivatele = int(input('Zadej cislo uzivatele:'))
-def cislo_uzivatele():
-    if cislo_uzivatele == 9 or cislo_uzivatele[:4] == predvolba:
+def vyhodnot_cislo(cislo_uzivatele):
+    if cislo_uzivatele == 9 or (cislo_uzivatele == 13 and cislo_uzivatele[:4] == predvolba):
         return True
-        text_zpravy = ('Zadej text zpravy:')
-        def text_zpravy():
-             cena_jedne_zpravy = text_jedne_zpravy
-             cena_zpravy = len(text_zpravy) // text_jedne_zpravy
     else:
         return False
+
+
+
+text_zpravy = input('Zadej text zpravy:')
+def cena_zpravy(zprava):
+    if len(zprava) % 180 == 0:
+        cena = (len(zprava) // 180) * 3
+    else:
+        cena = ((len(zprava) // 180) + 1) * 3
+    return cena
+print(cena_zpravy (text_zpravy))
+
+
+
+cislo_uzivatele = int(input('Zadej cislo uzivatele:'))
+if vyhodnot_cislo(cislo_uzivatele):
+		text_zpravy = input('Zadej text zpravy:')
+		print(cena_zpravy(text_zpravy))
+else:
+		print("spatne cislo")
